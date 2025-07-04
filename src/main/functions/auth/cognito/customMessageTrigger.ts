9bd40ab34db07bc1e257e07db700d6265ccbf2a5
Mat <mantonio@tangocode.com>
2025-07-03 22:10:42 -0300

    feat: render html template in the lambda

--- src/main/functions/auth/cognito/customMessageTrigger.ts
+++ src/main/functions/auth/cognito/customMessageTrigger.ts
@@ -1,11 +1,16 @@
 import type { CustomMessageTriggerEvent } from "aws-lambda";
 
+import { render } from "@react-email/render";
+import ForgotPassword from "@infra/emails/templates/ForgotPassword";
+
 export async function handler(event: CustomMessageTriggerEvent) {
     if (event.triggerSource === "CustomMessage_ForgotPassword") {
-        const code = event.request.codeParameter;
+        const confirmationCode = event.request.codeParameter;
+
+        const html = await render(ForgotPassword({ confirmationCode }));
 
         event.response.emailSubject = "🍏 foodiary | Recupere a sua conta!";
-        event.response.emailMessage = `O seu código de recuperação é: ${code}`;
+        event.response.emailMessage = html;
     }
 
     return event;
