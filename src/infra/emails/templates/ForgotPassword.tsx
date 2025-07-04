2c6cefd635f62485974ba899473806bafffe807c
Mat <mantonio@tangocode.com>
2025-07-03 21:20:09 -0300

    feat: install react-email to set up email templates

---
+++ src/infra/emails/templates/ForgotPassword.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import { Button, Html } from "@react-email/components";
+
+export default function ForgotPassword() {
+    return (
+        <Html>
+            {/* this button component is converted to a anchor tag */}
+            <Button>Click me</Button>
+        </Html>
+    );
+}
